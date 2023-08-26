4264dd170f0164698eb760c2b95b415a525e6080
Simon Howroyd <simon.howroyd@wearable.technology>
2023-08-26 14:03:07 +0100

    Added some tests

---
+++ run_tests.py
@@ -0,0 +1,17 @@
+#!./.venv/bin/python3
+import pathlib
+import pytest
+import sys
+
+if __name__ == "__main__":
+    path = pathlib.Path().resolve()
+
+    sys.exit(
+        pytest.main(
+            [
+                f'--cov={path}',
+                '--cov-report=html',
+                'tests'
+            ]
+        )
+    )
